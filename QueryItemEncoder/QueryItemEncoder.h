686cf24055123f4c44766e23e55bbd30cac30923
Benedek Kozma <cyberbeni@gmail.com>
2018-07-15 13:35:16 +0200

    Initial Commit

---
+++ QueryItemEncoder/QueryItemEncoder.h
@@ -0,0 +1,19 @@
+//
+//  QueryItemEncoder.h
+//  QueryItemEncoder
+//
+//  Created by Benedek Kozma on 2018. 07. 15..
+//  Copyright © 2018. Benedek Kozma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QueryItemEncoder.
+FOUNDATION_EXPORT double QueryItemEncoderVersionNumber;
+
+//! Project version string for QueryItemEncoder.
+FOUNDATION_EXPORT const unsigned char QueryItemEncoderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QueryItemEncoder/PublicHeader.h>
+
+
