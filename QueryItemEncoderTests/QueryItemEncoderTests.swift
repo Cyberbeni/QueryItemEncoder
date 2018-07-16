9fae62f44e6b0ba0f3af1e7017cee8bec02fc392
Benedek Kozma <cyberbeni@gmail.com>
2018-07-16 19:27:42 +0200

    updated tests

--- QueryItemEncoderTests/QueryItemEncoderTests.swift
+++ QueryItemEncoderTests/QueryItemEncoderTests.swift
@@ -9,46 +9,92 @@
 import XCTest
 
 class QueryItemEncoderTests: XCTestCase {
+    var encoder: QueryItemEncoder!
+    var input: SampleObject!
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        self.input = SampleObject(string: "test", date: Date(timeIntervalSince1970: 0), number: 1, decimal: Decimal(string: "1.99")!)
+        self.encoder = QueryItemEncoder()
+        self.encoder.dateEncodingStrategy = .iso8601
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testEncode() {
-        let input = SampleObject(string: "test", date: Date(timeIntervalSince1970: 0), number: 1, decimal: Decimal(string: "1.99")!)
-        let encoder = QueryItemEncoder()
-        encoder.dateEncodingStrategy = .iso8601
+    func test_encodeOutputCount() {
         do {
             let output: [URLQueryItem] = try encoder.encode(input)
             XCTAssert(output.count == 4)
+        } catch let error  {
+            XCTFail("QueryItemEncoder.encode threw error: \(error)")
+        }
+    }
+    
+    func test_string() {
+        do {
+            let output = try encoder.encode(self.input)
             if let string = output.first(where: { $0.name == "string" }) {
                 XCTAssertEqual(string.value, "test", "Wrong value encoded for key 'string'")
             } else {
                 XCTFail("No item found with key 'string'")
             }
+        } catch let error {
+            XCTFail("QueryItemEncoder.encode threw error: \(error)")
+        }
+    }
+    
+    func test_date() {
+        do {
+            let output = try encoder.encode(self.input)
             if let date = output.first(where: { $0.name == "date" }) {
                 XCTAssertEqual(date.value, "1970-01-01T00:00:00Z", "Wrong value encoded for key 'date'")
             } else {
                 XCTFail("No item found with key 'date'")
             }
+        } catch let error {
+            XCTFail("QueryItemEncoder.encode threw error: \(error)")
+        }
+    }
+    
+    func test_int() {
+        do {
+            let output = try encoder.encode(self.input)
             if let number = output.first(where: { $0.name == "number" }) {
                 XCTAssertEqual(number.value, "1", "Wrong value encoded for key 'number'")
             } else {
                 XCTFail("No item found with key 'number'")
             }
+        } catch let error {
+            XCTFail("QueryItemEncoder.encode threw error: \(error)")
+        }
+    }
+    
+    func test_decimal() {
+        do {
+            let output = try encoder.encode(self.input)
             if let decimal = output.first(where: { $0.name == "decimal" }) {
                 XCTAssertEqual(decimal.value, "1.99", "Wrong value encoded for key 'decimal'")
             } else {
                 XCTFail("No item found with key 'decimal'")
             }
-        } catch let error  {
+        } catch let error {
             XCTFail("QueryItemEncoder.encode threw error: \(error)")
+        }
+    }
+    
+    func test_array() {
+        let input = [self.input]
+        XCTAssertThrowsError(try encoder.encode(input), "QueryItemEncoder.encode didn't throw error for array input") { (error) in
+            print(error)
+        }
+    }
+    
+    func test_singleValue() {
+        let input = "test"
+        XCTAssertThrowsError(try encoder.encode(input), "QueryItemEncoder.encode didn't throw error for single value string input") { (error) in
+            print(error)
         }
     }
 }
